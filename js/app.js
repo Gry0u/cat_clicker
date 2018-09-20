548c5597a70322316c8f4f91c7daef9544f3087b
Gauthier Riou <gauthier.riou@airbus.com>
2018-09-20 14:57:45 +0200

    5 cats

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
-for (let i = 1; i < 3; i++) {
+for (let i = 1; i < 6; i++) {
   let container = document.createElement('div');
   container.setAttribute('class', 'cat_container');
 
   let img = document.createElement('img');
   img.setAttribute('class', 'picture');
-  img.setAttribute('src', `img/cat_small${i}.jpg`);
+  img.setAttribute('src', `img/cat${i}.jpg`);
 
   let counterHTML = document.createElement('div');
   counterHTML.setAttribute('class', 'counter');
