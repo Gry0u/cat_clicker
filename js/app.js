1a14d69a8e074fbf8bd332c508c93175e71b5cf4
Gauthier Riou <gauthier.riou@airbus.com>
2018-09-20 14:39:02 +0200

    feat: Count clicks with 2 cats pictures

--- js/app.js
+++ js/app.js
@@ -1,7 +1,21 @@
-const picture = document.querySelector('.picture');
-let counterHTML = document.querySelector('.counter')
-let counter = 0;
-picture.addEventListener('click', function() {
-  counter +=1;
-  counterHTML.innerText = counter;
-});
+for (let i = 1; i < 3; i++) {
+  let container = document.createElement('div');
+  container.setAttribute('class', 'cat_container');
+
+  let img = document.createElement('img');
+  img.setAttribute('class', 'picture');
+  img.setAttribute('src', `img/cat_small${i}.jpg`);
+
+  let counterHTML = document.createElement('div');
+  counterHTML.setAttribute('class', 'counter');
+
+  container.appendChild(img);
+  container.appendChild(counterHTML);
+  document.querySelector('body').appendChild(container);
+  let counteri = 0;
+  counterHTML.innerText = counteri;
+  img.addEventListener('click', function() {
+    counteri +=1;
+    counterHTML.innerText = counteri;
+  });
+}
