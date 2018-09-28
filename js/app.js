14be409b05a5ee1ec9c8c47bf0c7d7cde7a0ea79
Gry0u <gauthriou@protonmail.com>
2018-09-28 16:54:24 +0200

    Add admin menu

--- js/app.js
+++ js/app.js
@@ -6,7 +6,8 @@ let model = {
     {name:'Twins', clicks: 0, src: 'img/Twins.jpg'},
     {name:'Dan', clicks: 0, src: 'img/Dan.jpg'},
     {name:'Yuri', clicks: 0, src: 'img/Yuri.jpg'}
-  ]
+  ],
+  admin_menu_visible: false
 };
 
 let game = {
@@ -15,6 +16,7 @@ let game = {
     model.currentCat = model.cats[0];
     catListView.init();
     catView.init();
+    adminMenuView.init();
   },
 
   increment: function() {
@@ -32,9 +34,32 @@ let game = {
 
   setCurrentCat: function(cat) {
     model.currentCat = cat;
+  },
+
+  getVisibility: function() {
+    return model.admin_menu_visible;
+  },
+
+  showMenu: function() {
+    model.admin_menu_visible = true;
+    adminMenuView.render();
+  },
+
+  hideMenu: function() {
+    model.admin_menu_visible = false;
+    adminMenuView.render();
+  },
+
+  addCat: function() {
+    model.cats.push({
+      name: document.getElementById('name').value,
+      clicks: Number(document.getElementById('clicks').value),
+      src: document.getElementById('url').value
+    });
+    debugger
+    catListView.render();
   }
 };
-
 
 let catView = {
   init: function() {
@@ -91,6 +116,45 @@ let catListView = {
 
       //add button to HTML
       this.catListElem.appendChild(elem);
+    }
+  }
+};
+
+let adminMenuView = {
+  init: function() {
+    //get DOM elements
+    this.adminButton = document.getElementById('admin-button');
+    this.menu = document.getElementById('admin');
+    this.saveButton = document.getElementById('save-button');
+    this.cancelButton = document.getElementById('cancel-button');
+
+    //add event listeners
+
+    //admin button click
+    this.adminButton.addEventListener('click', function() {
+      //show menu
+      game.showMenu();
+    });
+
+    //save button click
+    this.saveButton.addEventListener('click', function() {
+      game.addCat();
+    });
+
+    //cancel button click
+    this.cancelButton.addEventListener('click', function() {
+      game.hideMenu();
+    });
+    //display
+    this.render();
+  },
+
+  render: function() {
+    //Show/Hide depending on visibility value
+    if (game.getVisibility()) {
+      this.menu.style.display = "block";
+    } else {
+      this.menu.style.display = "none";
     }
   }
 };
